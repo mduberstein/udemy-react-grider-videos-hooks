f57209cae6e3ff7070e008946e1ca37f0056715c
Michael Duberstein <dubermic@flutterint.com>
2022-07-18 16:02:26 -0400

    Videos: After Clip 120 Adding a Video Type

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,2 +1,17 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyB8CRZdeQvcLMQ8M_l0c8cGM-ReF1tZMSE';
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    type: 'video',
+    maxResults: 5,
+    key: KEY
+  }
+});
+
+
+
+
  
