0272a46a1a793005386b91669bb04f68ee1df32f
Michael Duberstein <dubermic@flutterint.com>
2022-07-20 21:19:11 -0400

    Videos: After Clip 132 Fixing a Few Warnings

--- src/components/App.js
+++ src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
 
   onVideoSelect = (video) => {
     this.setState({selectedVideo: video});
-    // console.log('From the App', video);
+    //  
   }
 
   onTermSubmit = async term => {
@@ -27,8 +27,16 @@ class App extends React.Component {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+        <div className="ui grid">
+          <div className="ui row">
+            <div className="eleven wide column">
+              <VideoDetail video={this.state.selectedVideo} />
+            </div>
+            <div className="five wide column">
+              <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,14 +1,16 @@
-import './VideoItem.css'
+import "./VideoItem.css";
 import React from "react";
 
-const VideoItem = ({video, onVideoSelect}) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} alt={video.snippet.description} />
+      <img
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+        alt={video.snippet.title}
+      />
       <div className="content">
-        <div className="header">
-          {video.snippet.title}
-        </div>
+        <div className="header">{video.snippet.title}</div>
       </div>
     </div>
   );
