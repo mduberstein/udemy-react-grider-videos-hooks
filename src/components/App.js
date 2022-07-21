844e5cdacd108291a032a1506f3e6df5cb55557c
Michael Duberstein <dubermic@flutterint.com>
2022-07-20 21:49:20 -0400

    Videos: After Clip 133 Default Video Selection

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,27 @@ import VideoDetail from "./VideoDetail";
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onVideoSelect = (video) => {
-    this.setState({selectedVideo: video});
-    //  
+    this.setState({selectedVideo: video}); 
   }
 
   onTermSubmit = async term => {
   // params property in request config is per https://axios-http.com/docs/req_config
   // the config object of the request is merged with the config object of axis instance
-  // i.e. the second arg of call to axios.create from unsplash.js
+  // i.e. the second arg of call to axios.create from youtube.js
     const response = await youtube.get("/search", {
       // q was from https://developers.google.com/youtube/v3/docs/search/list
       params: { q: term },
     });
     // console.log(response.data.items);
-    this.setState({videos: response.data.items});
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   render() {
